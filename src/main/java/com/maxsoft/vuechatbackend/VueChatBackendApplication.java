843a9c3efd3cfab173bf190c09cc4efd5f99625e
maxim-semenko <maks.semenko@gmail.com>
2023-06-14 13:47:48 +0300

    First init commit

---
+++ src/main/java/com/maxsoft/vuechatbackend/VueChatBackendApplication.java
@@ -0,0 +1,13 @@
+package com.maxsoft.vuechatbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VueChatBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VueChatBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/maxsoft/vuechatbackend/VueChatBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.maxsoft.vuechatbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VueChatBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
