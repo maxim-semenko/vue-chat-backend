75f30b95d3bf99112471bb3793b23280afd7583e
maxim-semenko <maks.semenko@gmail.com>
2023-07-04 23:28:32 +0300

    Add controllers

---
+++ src/main/java/com/maxsoft/vuechatbackend/controller/FIleController.java
@@ -0,0 +1,11 @@
+package com.maxsoft.vuechatbackend.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController()
+@RequestMapping("/files")
+@RequiredArgsConstructor
+public class FIleController {
+}
