e74a18b49d5a11952228d0c442e9af15fb9389cf
maxim-semenko <maks.semenko@gmail.com>
2023-08-11 15:35:58 +0300

    Added method findByUsername

--- src/main/java/com/maxsoft/vuechatbackend/repository/AccountRepository.java
+++ src/main/java/com/maxsoft/vuechatbackend/repository/AccountRepository.java
@@ -4,8 +4,11 @@ import com.maxsoft.vuechatbackend.entity.Account;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
 import java.util.UUID;
 
 @Repository
 public interface AccountRepository extends JpaRepository<Account, UUID> {
+
+    Optional<Account> findByUsername(String username);
 }
