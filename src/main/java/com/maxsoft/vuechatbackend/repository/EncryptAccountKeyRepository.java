dbbefa1b6fc5908b656e736bbc7474318ab6b4e8
maxim-semenko <maks.semenko@gmail.com>
2023-08-17 23:30:03 +0300

    Added method find by accountId and type

--- src/main/java/com/maxsoft/vuechatbackend/repository/EncryptAccountKeyRepository.java
+++ src/main/java/com/maxsoft/vuechatbackend/repository/EncryptAccountKeyRepository.java
@@ -1,12 +1,18 @@
 package com.maxsoft.vuechatbackend.repository;
 
 import com.maxsoft.vuechatbackend.entity.EncryptAccountKey;
+import com.maxsoft.vuechatbackend.entity.enums.EncryptKeyType;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import javax.transaction.Transactional;
+import java.util.Optional;
 import java.util.UUID;
 
 @Repository
+@Transactional
 public interface EncryptAccountKeyRepository extends JpaRepository<EncryptAccountKey, UUID> {
+
+    Optional<EncryptAccountKey> findByAccountIdAndType(UUID accountId, EncryptKeyType type);
 }
 
